2b713e8fd61b7eab8d01ae663709e390b23b83fa
mahakagarwal7 <mahakagrwl07@gmail.com>
2026-07-25 13:16:22 +0530

    feat: implement professional single-page web UI in public/index.html

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import { errorHandler } from './middleware/errorHandler';
 import csvRoutes from './routes/csv.routes';
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes will be added here
 app.use('/api/csv', csvRoutes);
